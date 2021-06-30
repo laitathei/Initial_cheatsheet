7ea2135dd00d994451439aeaa63cf46c2e851b36
laitathei <56575156+laitathei@users.noreply.github.com>
2021-06-30 15:01:52 +0800

    Create init_setup.sh

--- init_setup.sh
+++ init_setup.sh
@@ -11,6 +11,7 @@ echo "Download ROS-melodic version  "
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt-get install ros-melodic-desktop-full
+sudo apt install python-rosdep
 rosdep init
 rosdep update
 sudo apt-get install ros-melodic-catkin python-catkin-tools

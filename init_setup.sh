0fab6b8afa4718ce34cd9927552328be7595e9c7
laitathei <56575156+laitathei@users.noreply.github.com>
2021-06-30 15:43:47 +0800

    Update init_setup.sh

--- init_setup.sh
+++ init_setup.sh
@@ -11,7 +11,7 @@ echo "Download ROS-melodic version  "
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt-get install ros-melodic-desktop-full
-sudo apt-get install python-rosdep
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 sudo apt-get install python-pip
 rosdep init
 rosdep update
